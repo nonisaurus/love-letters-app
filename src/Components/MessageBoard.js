19f188e92d687110c0172011e86d9685b9e9b3ad
norazics <nora.zics@yahoo.co.uk>
2023-04-06 19:16:22 +0100

    changed making api calls

--- src/Components/MessageBoard.js
+++ src/Components/MessageBoard.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import MessagePost from './MessagePost';
+import { getAllMessages } from './apiMessages';
 
 class MessageBoard extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            comments: []
+        }
+    }
+
+    componentDidMount () {
+        getAllMessages()
+        .then((response) => {
+            console.log('resp', response)
+        })
+        .catch((error) => {
+            console.log('something went wrong', error)
+        })
+    }
+
+    handleSubmit = (event) => {
+
+    }
+
+
+
   render() {
+
     return (
       <div>
             <h1>Message Board</h1>
+            <MessagePost 
+            handleSubmit = {this.handleSubmit} 
+            comment = {this.state.comments}/>
       </div>
     );
   }
