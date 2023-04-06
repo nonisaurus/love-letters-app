b5df0b7037c426e5c0620fd88e1e75fd4c8628c4
norazics <nora.zics@yahoo.co.uk>
2023-04-06 10:36:37 +0100

    created basic static welcome page

---
+++ src/Components/Welcome.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+            <h1>Love Letters</h1>
+            <p>Love Letter is a card game introduced in May 2012 and designed by Seiji Kanai. Each player aims to deliver a love letter to the Princess with the assistance of relatives and acquaintances.</p>
+        </div>
+        <div>
+            <p>Sign in or Sign up here</p>
+        </div>
+        <div>
+            <p>@nonisaurus</p>
+            <div>
+                <p>instagram icon</p>
+                <p>linked in icon</p>
+                <p>github icon</p>
+            </div>
+        </div>
+        <div>
+            <img src=''></img>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
