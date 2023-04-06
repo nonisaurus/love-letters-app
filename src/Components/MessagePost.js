0faa85b18bfa6e1d0608b73667cfd93e49cebe16
norazics <nora.zics@yahoo.co.uk>
2023-04-06 19:14:22 +0100

    changed message post to show some basic text

---
+++ src/Components/MessagePost.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class MessagePost extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            inputValue: ''
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            inputValue: event.target.value
+        })
+    }
+
+    try = (event) => {
+        event.preventDefault();
+        console.log("hey it worked")
+        this.setState({
+            inputValue: ''
+        })
+    }
+
+  render() {
+    return (
+      <div>
+        <form>
+            <label>
+            Leave your comment here:
+            <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+            </label>
+            <input type="button" value="Post" onClick={this.try}/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default MessagePost;
