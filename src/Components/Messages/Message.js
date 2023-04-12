1bdb0fc7842a1b69028df751d1651ff6cd71e8da
norazics <nora.zics@yahoo.co.uk>
2023-04-12 10:32:21 +0100

    changes Message.js into class based component

--- src/Components/Messages/Message.js
+++ src/Components/Messages/Message.js
@@ -1,25 +1,60 @@
+import React, { Component } from "react";
 import DeleteBtn from "./DeleteBtn";
 import EditBtn from "./EditBtn";
 
-function Message(props) {
+class Message extends React.Component{
 
-    return (
-      <div>
-        <div>
-            <h2>{props.username}</h2>
-        </div>
-        <div>
-            <p>{props.comment}</p>
-        </div>
-        <div>
-          {localStorage.getItem("username") === props.username ? (< EditBtn id={props.id}/>) : null }
-          {localStorage.getItem("username") === props.username ? (< DeleteBtn   id={props.id}
-                                                                                deleteMessage={props.deleteMessage}/>) : null }
-            
-            
-        </div>
-      </div>
-    );
+  constructor(props){
+      super(props)
+      this.state = {
+          buttonName: 'edit',
+          commentField: '',
+          isDisabled: 'yes'
+      }
+  }
+
+  handleCommentInput = (event) => {
+    this.setState({
+      commentField: event.target.value
+    })
+  }
+
+  updateButton = (changeButtonTo) => {
+     if (changeButtonTo === 'edit'){
+      this.setState({
+        buttonName: 'edit'
+      })
+    }else if ( changeButtonTo === 'save'){
+      this.setState({
+        buttonName: 'save'
+      })
+    }
+  }
+
+  render () {
+        return (
+        <>
+          <form>
+            <label>
+              <h2>{this.props.username}</h2>
+              <input type="text" name="name" disabled={this.state.buttonName === 'edit' ? true : false} defaultValue={this.props.comment} onChange={this.handleCommentInput} />
+            </label>
+          </form>
+          <div>
+            {localStorage.getItem("username") === this.props.username ? 
+            (< EditBtn  id={this.props.id}
+                        comment={this.state.commentField}
+                        updateMessage={this.props.updateMessage}
+                        updateButton={this.updateButton}
+                        buttonName={this.state.buttonName}
+                        />) : null }
+            {localStorage.getItem("username") === this.props.username ? (
+            < DeleteBtn  id={this.props.id}
+                         deleteMessage={this.props.deleteMessage}/>) : null }
+          </div>
+        </>
+      );
+    }
   }
   
 export default Message;
